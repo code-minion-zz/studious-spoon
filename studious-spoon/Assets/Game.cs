63870f25fc647bc301cf755dd0bf8ef02075b5f6
Event <event@mds.co.nz>
2017-01-21 15:05:17 +1300

    some ui and some node spawning

---
+++ studious-spoon/Assets/Game.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    public Sprite spriteToUse;
+    [SerializeField]
+    GameObject nodeRoot;
+
+	// Use this for initialization
+	void Start () {
+        Setup();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Setup()
+    {
+        nodeRoot = GameObject.Find("Nodes");
+        if (!nodeRoot)
+        {
+            nodeRoot = MakeNodes(10, 10);
+        }
+    }
+
+    GameObject MakeNodes(int col, int row)
+    {
+        var nodeContainer = new GameObject("Nodes");
+        var roottrans = nodeContainer.transform;
+        roottrans.position = new Vector3(-7.5f, 4.5f);
+        for (int i = 0; i < col; ++i)
+        {
+            var rowLabel = i.ToString("00");
+            var rowContainer = new GameObject("Row" + rowLabel);
+            rowContainer.transform.parent = roottrans;
+            rowContainer.transform.localPosition = new Vector3(i, 0, 0);
+            for (int j = 0; j < row; ++j)
+            {
+                var node = new GameObject("Node"+ rowLabel + j, typeof(SpriteController), typeof(Node));
+                node.transform.parent = rowContainer.transform;
+                node.transform.localPosition = new Vector3(0, -j, 0);
+                node.AddComponent<SpriteRenderer>().sprite = spriteToUse;
+            }
+
+        }
+
+        return nodeContainer;
+    }
+}
